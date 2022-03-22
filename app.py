0359db7212e72246058359fb9fc6c283ba983828
lichangao1826 <lca1826@163.com>
2022-03-22 15:31:07 +0800

    up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from search import Search
 
 app = Flask(__name__)
@@ -14,3 +14,10 @@ def hello_world():
 def init_vector():
     search.init_vector()
     return "finished!"
+
+
+@app.route("/similar_word", methods=["POST"])
+def similar_word():
+    params = request.json
+    words = search.query_word(params["word"], params["top_k"])
+    return jsonify(words)
